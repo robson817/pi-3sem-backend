25fb4439ab8c680f872f14f95d6640d39bd26b4a
Robson Alan Mantovani <robsonamantovani@gmail.com>
2025-05-27 10:17:42 -0300

    feat: remove Swagger integration and related dependencies from the project

--- cozinhai/src/main.ts
+++ cozinhai/src/main.ts
@@ -5,7 +5,6 @@ import {
     ValidationError,
     ValidationPipe,
 } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -38,28 +37,6 @@ async function bootstrap() {
             },
         }),
     );
-
-    // Configuração do Swagger
-    const config = new DocumentBuilder()
-        .setTitle('Cozinhai - Backend API')
-        .setDescription(
-            'Esta API fornece os serviços de backend para uma plataforma voltada à redução do desperdício de alimentos. Ela é responsável pelo gerenciamento de usuários (incluindo autenticação opcional), além de permitir que usuários autenticados possam favoritar receitas, comentar e avaliá-las. Comentários e avaliações podem ser visualizados por qualquer usuário, autenticado ou não. A API não realiza integração direta com serviços externos de receitas, mas oferece suporte às funcionalidades sociais e personalizadas da aplicação.',
-        )
-        .setVersion('0.1.0')
-        .addTag('Usuarios', 'Endpoints relacionados a usuários')
-        .addTag('Receitas', 'Endpoints relacionados a receitas')
-        .addTag('Avaliações', 'Endpoints relacionados a avaliações de receitas')
-        .setContact(
-            'Robson Alan Mantovani',
-            'https://github.com/robson817',
-            'robsonamantovani@gmail.com',
-        )
-        .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-
-    await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
