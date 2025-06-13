40fe0d328f8fe0e5052a98ca106b3f1cb7fee3cf
Robson Alan Mantovani <robsonamantovani@gmail.com>
2025-06-13 20:29:02 -0300

    feat: integrate UsersModule into RecipesModule and export MongooseModule from UsersModule

--- cozinhai/src/recipes/recipes.module.ts
+++ cozinhai/src/recipes/recipes.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RecipesService } from './recipes.service';
 import { RecipesController } from './recipes.controller';
 import { Recipe, RecipesSchema } from './recipe.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: Recipe.name, schema: RecipesSchema },
         ]),
+        UsersModule,
     ],
     controllers: [RecipesController],
     providers: [RecipesService],
--- cozinhai/src/users/users.module.ts
+++ cozinhai/src/users/users.module.ts
@@ -17,6 +17,6 @@ import { JwtModule } from '@nestjs/jwt';
     ],
     controllers: [UsersController],
     providers: [UsersService],
-    exports: [UsersService],
+    exports: [UsersService, MongooseModule],
 })
 export class UsersModule {}
